76ee69fc4e64f20341ba4e232fdc04918532d1b8
RadhulSics <radhul.sics@gmail.com>
2024-06-29 15:40:57 +0530

    80%

---
+++ Advocates/advocateController.js
@@ -0,0 +1,546 @@
+const Advocate = require('./advocateSchema');
+const jwt = require('jsonwebtoken');
+const secret = 'advocate'; 
+
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, "./upload");
+  },
+  filename: function (req, file, cb) {
+    const uniquePrefix = 'prefix-'; 
+    const originalname = file.originalname;
+    const extension = originalname.split('.').pop();
+    const filename = uniquePrefix + originalname.substring(0, originalname.lastIndexOf('.')) + '-' + Date.now() + '.' + extension;
+    cb(null, filename);
+  },
+});
+const upload = multer({ storage: storage }).array("files", 2);
+
+const uploadProfile = multer({ storage: storage }).single('profilePic');
+
+// Register Advocate
+const registerAdvocate = async (req, res) => {
+    try {
+        const { name, bcNo, bcState, contact, email, password, gender, address, experience, nationality, qualification, dob, professionalExperience, dateOfEnrollment, specialization } = req.body;
+
+        const newAdvocate = new Advocate({
+            name,
+            bcNo,
+            bcState,
+            contact,
+            email,
+            password,
+            gender,
+            address,
+            experience,
+            nationality,
+            qualification,
+            dob,
+            professionalExperience,
+            dateOfEnrollment,
+            specialization,
+            idProof:req.files[1],
+            profilePic:req.files[0]
+        });
+
+        let existingAdvocate = await Advocate.findOne({ bcNo });
+        let existingAdvocate2 = await Advocate.findOne({ contact });
+        if (existingAdvocate) {
+            return res.json({
+                status: 409,
+                msg: "BarCouncil Enrollment Number Already Registered With Us !!",
+                data: null
+            });
+        }
+        else if(existingAdvocate2) {
+            return res.json({
+                status: 409,
+                msg: "Contact Number Already Registered With Us !!",
+                data: null
+            });
+        }
+        await newAdvocate.save()
+            .then(data => {
+                return res.json({
+                    status: 200,
+                    msg: "Inserted successfully",
+                    data: data
+                });
+            })
+            .catch(err => {
+                if (err.code === 11000) {
+                    return res.json({
+                        status: 409,
+                        msg: "Email already in use",
+                        data: err
+                    });
+                }
+                return res.json({
+                    status: 500,
+                    msg: "Data not Inserted",
+                    data: err
+                });
+            });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// View all advocates
+const viewAdvocates = (req, res) => {
+    Advocate.find({adminApproved:true})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+
+// View all advocates
+const viewAdvocatesBySpecializn = (req, res) => {
+    Advocate.find({specialization:req.body.specialization})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+// View all advocate Reqs
+const viewAdvocateReqs = (req, res) => {
+    Advocate.find({adminApproved:false})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+
+// approve Advocate
+const approveAdvocateById = (req, res) => {
+    Advocate.findByIdAndUpdate({_id:req.params.id},{adminApproved:true})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+
+// approve Advocate
+const activateAdvocateById = (req, res) => {
+    Advocate.findByIdAndUpdate({_id:req.params.id},{isActive:true})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+
+// approve Advocate
+const deactivateAdvocateById = (req, res) => {
+    Advocate.findByIdAndUpdate({_id:req.params.id},{isActive:false})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data obtained successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+
+// reject Advocate
+const rejectAdvocateById = (req, res) => {
+    Advocate.findByIdAndDelete({_id:req.params.id})
+        .exec()
+        .then(data => {
+            if (data.length > 0) {
+                res.json({
+                    status: 200,
+                    msg: "Data Removed successfully",
+                    data: data
+                });
+            } else {
+                res.json({
+                    status: 200,
+                    msg: "No Data obtained"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not obtained",
+                Error: err
+            });
+        });
+};
+
+// Update advocate by ID
+const editAdvocateById = async (req, res) => {
+    const { name, bcNo, bcState, contact, email, password, gender, address, experience, nationality, qualification, dob, professionalExperience, dateOfEnrollment, specialization } = req.body;
+console.log("profilePic",req.body.filename);
+    Advocate.findByIdAndUpdate({ _id: req.params.id }, {
+        name,
+        bcNo,
+        bcState,
+        contact,
+        email,
+        password,
+        gender,
+        address,
+        experience,
+        nationality,
+        qualification,
+        dob,
+        professionalExperience,
+        dateOfEnrollment,
+        specialization,
+        profilePic:req.file
+    })
+        .exec()
+        .then(data => {
+            res.json({
+                status: 200,
+                msg: "Updated successfully"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not Updated",
+                Error: err
+            });
+        });
+};
+
+// View advocate by ID
+const viewAdvocateById = (req, res) => {
+    Advocate.findById({ _id: req.params.id })
+        .exec()
+        .then(data => {
+            res.json({
+                status: 200,
+                msg: "Data obtained successfully",
+                data: data
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "No Data obtained",
+                Error: err
+            });
+        });
+};
+
+// Delete advocate by ID
+const deleteAdvocateById = (req, res) => {
+    Advocate.findByIdAndUpdate({ _id: req.params.id },{isActive:'inactive'})
+        .exec()
+        .then(data => {
+            res.json({
+                status: 200,
+                msg: "Data removed successfully",
+                data: data
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "No Data obtained",
+                Error: err
+            });
+        });
+};
+
+// Forgot Password for advocate
+const forgotPassword = (req, res) => {
+    Advocate.findOneAndUpdate({ email: req.body.email }, {
+        password: req.body.password
+    })
+        .exec()
+        .then(data => {
+            if (data != null)
+                res.json({
+                    status: 200,
+                    msg: "Updated successfully"
+                });
+            else
+                res.json({
+                    status: 500,
+                    msg: "User Not Found"
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not Updated",
+                Error: err
+            });
+        });
+};
+
+// Reset Password for advocate
+const resetPassword = async (req, res) => {
+    let pwdMatch = false;
+
+    await Advocate.findById({ _id: req.params.id })
+        .exec()
+        .then(data => {
+            if (data.password === req.body.oldpassword)
+                pwdMatch = true;
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                msg: "Data not Updated",
+                Error: err
+            });
+        });
+
+    if (pwdMatch) {
+        await Advocate.findByIdAndUpdate({ _id: req.params.id }, {
+            password: req.body.newpassword
+        })
+            .exec()
+            .then(data => {
+                if (data != null)
+                    res.json({
+                        status: 200,
+                        msg: "Updated successfully"
+                    });
+                else
+                    res.json({
+                        status: 500,
+                        msg: "User Not Found"
+                    });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    status: 500,
+                    msg: "Data not Updated",
+                    Error: err
+                });
+            });
+    } else {
+        res.json({
+            status: 405,
+            msg: "Your Old Password doesn't match"
+        });
+    }
+};
+
+const createToken = (user) => {
+    return jwt.sign({ userId: user._id }, secret, { expiresIn: '1h' });
+};
+
+//advocate login
+const login = (req, res) => {
+    const { email, password } = req.body;
+
+    Advocate.findOne({ email }).then(user => {
+
+        if (!user) {
+            return res.json({ status: 405, msg: 'User not found' });
+        }
+
+        if (user.password != password) {
+            return res.json({ status: 405, msg: 'Password Mismatch !!' });
+        }
+        if (user.adminApproved==false ||user.isActive==false) {  
+                      return res.json({ status: 405, msg: 'Please get Approval From Admin!!' });
+        }
+        const token = createToken(user);
+
+        res.json({
+            status: 200,
+            data: user,
+            token
+        });
+
+    }).catch(err => {
+        console.log(err);
+        return res.json({ status: 500, msg: 'Something went wrong' });
+    })
+};
+
+// Validate
+const requireAuth = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+        return res.json({ status: 401, msg: 'Unauthorized' });
+    }
+    jwt.verify(token, secret, (err, decodedToken) => {
+        if (err) {
+            return res.json({ status: 401, msg: 'Unauthorized', err: err });
+        }
+
+        req.user = decodedToken.userId;
+        next();
+    });
+};
+
+
+//
+const addRating = (req, res) => {
+    let newRate = parseInt(req.body.rating);
+    let rating = 0;
+    Advocate.findById({ _id: req.params.id })
+      .exec()
+      .then((data) => {
+        rating = data.rating;
+        if (data.rating != 0) rating = (rating + newRate) / 2;
+        else rating = newRate;
+        Advocate.findByIdAndUpdate(
+          { _id: req.params.id },
+          {
+            rating: rating,
+          },
+          { new: true }
+        )
+          .exec()
+          .then((data) => {
+            res.json({
+              status: 200,
+              msg: "Data obtained successfully",
+              data: data,
+            });
+          })
+          .catch((err) => {
+            res.json({
+              status: 500,
+              msg: "Data not Inserted",
+              Error: err,
+            });
+          });
+      });
+  };
+  
+module.exports = {
+    registerAdvocate,
+    viewAdvocates,
+    editAdvocateById,
+    viewAdvocateById,
+    deleteAdvocateById,
+    forgotPassword,
+    resetPassword,
+    login,
+    requireAuth,
+    upload,
+    viewAdvocateReqs,
+    approveAdvocateById,
+    rejectAdvocateById,
+    activateAdvocateById,
+    deactivateAdvocateById,
+    uploadProfile,
+    viewAdvocatesBySpecializn,
+    addRating
+};
---
+++ routes.js
@@ -0,0 +1,112 @@
+const router=require('express').Router()
+const user=require('./User/userController')
+const advocates=require('./Advocates/advocateController')
+const cases=require('./Cases/caseController')
+const appointments=require('./AdvAppointments/appointmentController')
+ const chat=require('./Chats/chatController')
+const caseStatusController=require('./Cases/CaseStatus/caseStatusController')
+const Payments=require('./Cases/Payments/paymentController')
+const Evidences=require('./Cases/Evidences/evidenceController')
+const blog=require('./Blogs/blogController')
+const complaints=require('./complaints/complaintController')
+const Reviews=require('./Reviews/reviewController')
+
+
+//advocate routes
+router.post('/registerAdvocate',advocates.upload,advocates.registerAdvocate)
+router.post('/viewAdvocateById/:id',advocates.viewAdvocateById)
+router.post('/forgotPassword',advocates.forgotPassword)
+router.post('/loginAdvocate',advocates.login)
+router.post('/editAdvocateById/:id',advocates.uploadProfile,advocates.editAdvocateById)
+router.post('/deleteAdvocateById/:id',advocates.deleteAdvocateById)
+router.post('/resetPassword/:id',advocates.resetPassword)
+router.post('/approveAdvocateById/:id',advocates.approveAdvocateById)
+router.post('/rejectAdvocateById/:id',advocates.rejectAdvocateById)
+router.post('/requireAuth',advocates.requireAuth)
+router.post('/viewAdvocateReqs',advocates.viewAdvocateReqs)
+router.post('/viewAdvocates',advocates.viewAdvocates)
+router.post('/activateAdvocateById/:id',advocates.activateAdvocateById)
+router.post('/deactivateAdvocateById/:id',advocates.deactivateAdvocateById)
+router.post('/viewAdvocatesBySpecializn',advocates.viewAdvocatesBySpecializn)
+router.post('/addRating/:id',advocates.addRating)
+
+
+
+
+//users routers
+router.post('/registerUser',user.upload,user.registerUser)
+router.post('/loginUser',user.login)
+router.post('/editUserById/:id',user.upload,user.editUserById)
+router.post('/viewUserById/:id',user.viewUserById)
+router.post('/userforgotPassword',user.forgotPassword)
+router.post('/resetPassword',user.resetPassword)
+router.post('/deleteUserById/:id',user.deleteUserById)
+router.post('/editUserById',user.upload,user.login)
+router.post('/requireAuth',user.requireAuth)
+router.post('/viewUsers',user.viewUsers)
+
+//case routes
+router.post('/createCase/:id',cases.upload,cases.createCase)
+router.post('/getCaseType',cases.getCaseType)
+router.post('/getCaseByUserId/:id',cases.getCaseByUserId)
+router.post('/getCaseById/:id',cases.getCaseById)
+router.post('/deleteCase/:id',cases.deleteCase)
+
+
+router.post('/createAppointment',appointments.createAppointment)
+router.post('/getAppointmentReqsByUserId/:id',appointments.getAppointmentReqsByUserId)
+router.post('/getAppointmentReqsForAdv/:id',appointments.getAppointmentReqsForAdv)
+router.post('/acceptReqbyAdv/:id',appointments.acceptReqbyAdv)
+router.post('/rejectReqbyAdv/:id',appointments.rejectReqbyAdv)
+router.post('/getAppointmentReqsById/:id',appointments.getAppointmentReqsById)
+router.post('/getApprovedAppointmentsForAdv/:id',appointments.getApprovedAppointmentsForAdv)
+router.post('/getApprovedAppointmentsForAdv/:id',appointments.getApprovedAppointmentsForAdv)
+
+//chatting
+router.post('/chatting',chat.chatting)
+router.post('/viewChatRecipientsforAdvocateById/:id',chat.viewChatRecipientsforAdvocateById)
+router.post('/viewChatRecipientsforUserId/:id',chat.viewChatRecipientsforUserId)
+router.post('/viewChatBetweenUserAndAdv',chat.viewChatBetweenUserAndAdv)
+
+
+
+
+//case status
+router.post('/createStatus/:id',caseStatusController.createStatus)
+router.post('/getStatusById/:id',caseStatusController.getStatusById)
+router.post('/getStatusByCaseId/:id',caseStatusController.getStatusByCaseId)
+
+//payments
+router.post('/reqPayment/:id',Payments.reqPayment)
+router.post('/getAllPaymentByAdvId/:id',Payments.getAllPaymentByAdvId)
+router.post('/getPaymentsByCaseId/:id',Payments.getPaymentsByCaseId)
+router.post('/getPaymentsById/:id',Payments.getPaymentsById)
+router.post('/receivePaymentsById/:id',Payments.receivePaymentsById)
+
+//evidences
+router.post('/addEvidence/:id',Evidences.upload,Evidences.addEvidence)
+router.post('/getEvidenceByCaseId/:id',Evidences.getEvidenceByCaseId)
+router.post('/getEvidenceById/:id',Evidences.getEvidenceById)
+
+
+//blogs
+router.post('/addBlog/:id',blog.upload,blog.addBlog)
+router.post('/viewBlogsById/:id',blog.viewBlogsById)
+router.post('/editBlogsById/:id',blog.upload,blog.editBlogsById)
+router.post('/deleteBlogsById/:id',blog.deleteBlogsById)
+router.post('/viewAllBlogs',blog.viewAllBlogs)
+router.post('/viewMyBlogsByadvocateId/:id',blog.viewMyBlogsByadvocateId)
+
+
+//blogs
+router.post('/addComplaint',complaints.addcomplaint)
+router.post('/viewAllComplaints',complaints.viewAllcomplaints)
+router.post('/viewComplaintById/:id',complaints.viewcomplaintById)
+router.post('/deleteComplaintById/:id',complaints.deletecomplaintById)
+
+//reviews
+router.post('/addReview',Reviews.addReview)
+router.post('/viewAllreviewsByAdvId/:id',Reviews.viewAllreviewsByAdvId)
+
+
+module.exports=router
